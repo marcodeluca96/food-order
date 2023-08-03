88b4729a0f8653c77a158651297ccce780863062
Marco De Luca <marco.deluca@weavesrl.com>
2023-08-03 13:21:27 +0200

    fix refresh routes

--- src/firebase-message.js
+++ src/firebase-message.js
@@ -8,7 +8,7 @@ export const getTokenFunc = (setTokenFound) => {
   })
     .then((currentToken) => {
       if (currentToken) {
-        // console.log('current token for client: ', currentToken);
+        console.log('current token for client: ', currentToken);
         setTokenFound(true);
         // Track the token -> client mapping, by sending to backend server
         // show on the UI that permission is secured
